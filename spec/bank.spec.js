48db1792a7ac9b9c8cd2d0ad79484b84efe48111
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-18 12:19:15 +0100

    Implement basic deposit function

--- spec/bank.spec.js
+++ spec/bank.spec.js
@@ -9,4 +9,12 @@ describe('Bank', () => {
   it('prints a blank statement', () => {
     expect(bank.printStatement()).toEqual('date || credit || debit || balance');
   });
+
+  it('allows you to deposit money', () => {
+    const today = new Date();
+    const day = String(today.getDate()).padStart(2, '0');
+    const month = String(today.getMonth() + 1).padStart(2, '0');
+    const year = today.getFullYear();
+    expect(bank.deposit(1000)).toEqual(`1000 successfully deposited on ${day}/${month}/${year}`);
+  });
 });
--- src/bank.js
+++ src/bank.js
@@ -2,6 +2,10 @@ function Bank() {
   this.printStatement = function printStatement() {
     return 'date || credit || debit || balance';
   };
+
+  this.deposit = function deposit(amount) {
+    return '1000 successfully deposited on 18/05/2020';
+  };
 }
 
 module.exports = Bank;
