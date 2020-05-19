89b81af6450636a7ec94ac7e002b49e762d072b1
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-19 14:54:46 +0100

    Refactor Deposit tests
    I removed tests that were effectively duplicating the units tests
    already done on the isValidAmount function.
    
    The tests were replaced by two simple tests that check for the
    correct response when the amount is valid and invalid respectively.

--- spec/deposit.spec.js
+++ spec/deposit.spec.js
@@ -1,61 +1,50 @@
 const Deposit = require('../src/deposit.js');
 
 describe('Deposit', () => {
-  let date;
-  let dateString;
+  let date = new Date();
+  let deposit;
 
-  beforeEach(() => {
-    jasmine.clock().install();
-    jasmine.clock.mockDate;
-
-    date = new Date();
-    const day = String(date.getDate()).padStart(2, '0');
-    const month = String(date.getMonth() + 1).padStart(2, '0');
-    const year = date.getFullYear();
-    dateString = `${day}/${month}/${year}`;
+  describe('valid deposits', () => {
+    it('allows you to deposit a valid amount', () => {
+      function isValidAmount() { return true; }
+      const details = { date, amount: 3000, balance: 0 };
+      expect(() => { deposit = new Deposit(details, isValidAmount); }).not.toThrow();
+    });
   });
 
-  afterEach(() => {
-    jasmine.clock().uninstall();
-  });
-
-  it('allows you to deposit 1000 pounds', () => {
-    let deposit;
-    const details = { date, amount: 3000, balance: 0 };
-    expect(() => { deposit = new Deposit(details); }).not.toThrow();
-  });
-
-  describe('errors', () => {
-    let deposit;
-    it('refuses deposits unless they are positive', () => {
+  describe('invalid deposits', () => {
+    it('refuses a deposit when the amount is invalid', () => {
+      function isValidAmount() { return false; }
       const details = { date, amount: 0, balance: 0 };
-      expect(() => { deposit = new Deposit(details); }).toThrow(new Error('Unable to make deposit - amount is invalid'));
-    });
-
-    it('refuses deposits with too many decimal places', () => {
-      const details = { date, amount: 100.321, balance: 0 };
-      expect(() => { deposit = new Deposit(details); }).toThrow(new Error('Unable to make deposit - amount is invalid'));
-    });
-
-    it('refuses deposits that arent numbers', () => {
-      const details = { date, amount: 'not a number', balance: 0 };
-      expect(() => { deposit = new Deposit(details); }).toThrow(new Error('Unable to make deposit - amount is invalid'));
-    });
-
-    it('refuses deposits that are numbers as strings', () => {
-      const details = { date, amount: '100', balance: 0 };
-      expect(() => { deposit = new Deposit(details); }).toThrow(new Error('Unable to make deposit - amount is invalid'));
+      expect(() => { deposit = new Deposit(details, isValidAmount); }).toThrow(new Error('Unable to make deposit - amount is invalid'));
     });
   });
 
   describe('.printStatementLine', () => {
+    let dateString;
+
+    beforeEach(() => {
+      jasmine.clock().install();
+      jasmine.clock.mockDate;
+
+      date = new Date();
+      const day = String(date.getDate()).padStart(2, '0');
+      const month = String(date.getMonth() + 1).padStart(2, '0');
+      const year = date.getFullYear();
+      dateString = `${day}/${month}/${year}`;
+    });
+
+    afterEach(() => {
+      jasmine.clock().uninstall();
+    });
+
     it('prints the line for the deposit', () => {
-      const deposit = new Deposit({ date, amount: 3000, balance: 0 });
+      deposit = new Deposit({ date, amount: 3000, balance: 0 });
       expect(deposit.printStatementLine()).toEqual(`${dateString} || 3000.00 || || 3000.00`);
     });
 
     it('prints the line for a decimal deposit', () => {
-      const deposit = new Deposit({ date, amount: 3000.12, balance: 0 });
+      deposit = new Deposit({ date, amount: 3000.12, balance: 0 });
       expect(deposit.printStatementLine()).toEqual(`${dateString} || 3000.12 || || 3000.12`);
     });
   });
