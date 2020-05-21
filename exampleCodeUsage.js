78343b7ffc733a80d11f34c564f1a9af09ea3ffd
Phil Vigus <philipvigus@yahoo.co.uk>
2020-05-21 14:54:12 +0100

    Update exampleCodeUsage.js
    A successful deposit was added so that the example shows all
    possible interactions. Printed labels were also added to split
    the output up a little and make it easier to read.

--- exampleCodeUsage.js
+++ exampleCodeUsage.js
@@ -1,8 +1,15 @@
 import Account from './src/account.js';
 
 const account = new Account();
+console.log('\nAccount.printStatement() for an empty statement');
 console.log(account.printStatement());
+console.log('\nAccount.deposit() and Account.withdraw() successes');
 console.log(account.deposit(100));
 console.log(account.deposit(323.11));
-console.log(account.withdraw(-100));
+console.log(account.withdraw(300.04));
+console.log('\nAccount.deposit() and Account.withdraw() errors');
+console.log(account.deposit(-100));
+console.log(account.withdraw(0));
+console.log(account.withdraw(50000));
+console.log('\nAccount.printStatement() for a statement with transactions');
 console.log(account.printStatement());
